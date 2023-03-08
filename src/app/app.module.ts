c0f38916e8b274a93895c4a1278d943808049e60
olehsemkiv <olegsemkiv3065@gmail.com>
2023-03-08 14:45:31 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,50 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DiscountComponent } from './pages/discount/discount.component';
+import { DiscountInfoComponent } from './pages/discount-info/discount-info.component';
+import { ProductComponent } from './pages/product/product.component';
+import { ProductInfoComponent } from './pages/product-info/product-info.component';
+import { DeliveryComponent } from './pages/delivery/delivery.component';
+import { AboutComponent } from './pages/about/about.component';
+import { NewsComponent } from './pages/news/news.component';
+import { FaqComponent } from './pages/faq/faq.component';
+import { OfertaComponent } from './pages/oferta/oferta.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdminCategoryComponent } from './admin/admin-category/admin-category.component';
+import { AdminProductComponent } from './admin/admin-product/admin-product.component';
+import { AdminDiscountComponent } from './admin/admin-discount/admin-discount.component';
+import { AdminNewsComponent } from './admin/admin-news/admin-news.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    DiscountComponent,
+    DiscountInfoComponent,
+    ProductComponent,
+    ProductInfoComponent,
+    DeliveryComponent,
+    AboutComponent,
+    NewsComponent,
+    FaqComponent,
+    OfertaComponent,
+    ContactsComponent,
+    CheckoutComponent,
+    AdminComponent,
+    AdminCategoryComponent,
+    AdminProductComponent,
+    AdminDiscountComponent,
+    AdminNewsComponent,
+    AdminOrdersComponent
   ],
   imports: [
     BrowserModule,
